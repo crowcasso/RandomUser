e1628c096902f10bb798cb060e3676ae125d6ef6
crowcasso <crowcasso@gmail.com>
2017-11-03 08:43:59 -0400

    Initial Commit

---
+++ RandomUser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by Joel Hollingsworth on 11/3/17.
+//  Copyright © 2017 Joel Hollingsworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
